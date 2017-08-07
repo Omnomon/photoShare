7947e2916c6c4d1f6858158cc79fc05dec809321
Omnomon <jessselu@gmail.com>
2017-08-07 00:29:37 -0700

    rough test of sql for creator and album. added creator post route

--- routes/creator-routes.js
+++ routes/creator-routes.js
@@ -2,4 +2,12 @@ var db = require("../models");
 
 module.exports = function(app){
 	console.log("empty for now")
+
+	// POST route to make creator 
+	app.post("/api/creator", (req, res)=>{
+		db.creator.create(req.body).then(addCreator=>{
+			res.json(addCreator)
+		})
+	}) 
+
 }
--- routes/post-routes.js
+++ routes/post-routes.js
@@ -1,23 +1,41 @@
 var db = require("../models");
 
-module.exports = function(app){
-	//GET route for one photo
-	app.get("/api/album/:albumId/post/:photoId", (req, res)=>{
-		db.post.findOne({
-			where: {
-				photoId: req.body.photoId
-			}, 
-			include: [db.album]
-		}).then(dbPhoto=>{
-			res.json(dbPhoto)
-		})
-	})
+module.exports = function(app) {
+    //GET route for one photo
+    app.get("/api/album/albumId/post/:id", (req, res) => {
+        db.post.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.album]
+        }).then(dbPhoto => {
+            res.json(dbPhoto)
+        })
+    })
 
+    //POST route for adding a photo to album
+    app.post("/api/album/albumId/post/", (req, res) => {
+        db.post.create(req.body).then(addPhoto => {
+            res.json(addPhoto)
+        })
+    })
 
-	//POST route for adding photos to album
-	app.post("/api/album/albumId:/post/:photoId", (req, res)=>{
-		db.post.create(req.body).then(addPhoto=>{
-			res.json(addPhoto)
-		})
-	})
+    //PUT route for updating a photo 
+    app.put("/api/album/albumId/post/", (req, res) => {
+        db.post.update(req.body, {
+            where: {
+                id: req.body.id
+            }
+        }).then(dbPost => {
+            res.json(dbPost)
+        })
+    })
+
+    //DELETE route for deleting a photo
+    app.delete("/api/album/albumId/post/:id", (req, res) => {
+        db.post.destroy({}).then(dbPost => {
+            res.json(dbPost)
+        })
+    })
+
 }
